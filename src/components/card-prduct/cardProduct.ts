5540bbe8b9764b58e471d4f1fdbeaf5f0ca24874
Anna-Grigencha <grigencha10@gmail.com>
2023-01-09 08:12:29 +0300

    feat: modified styles in filtres

--- src/components/card-prduct/cardProduct.ts
+++ src/components/card-prduct/cardProduct.ts
@@ -5,17 +5,17 @@ import BlockCardInfo from './card-info';
 import CardButton from './card-button';
 
 
-class Card extends Block{
-    
-    constructor(id: string, className: string) {
-        super(id, className);
-      }
-    
-      render(i:number): HTMLElement {
-        this.container.append((new CardImg('card__img')).render(i));
-        this.container.append((new BlockCardInfo('id_card__info', 'card__info')).render(i));
-        this.container.append((new CardButton('id_card_button', 'card__button').render(i)));
-        return this.container;
-      }
-    }
-    export default Card;
+class Card extends Block {
+
+  constructor(id: string, className: string) {
+    super(id, className);
+  }
+
+  render(i: number): HTMLElement {
+    this.container.append((new CardImg('card__img')).render(i));
+    this.container.append((new BlockCardInfo('id_card__info', 'card__info')).render(i));
+    this.container.append((new CardButton('id_card_button', 'card__button').render(i)));
+    return this.container;
+  }
+}
+export default Card;
