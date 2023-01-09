23ba6402bb42e93983e4c4dc1d5c19107d853744
chelovek-zeml <stranger37561@gmail.com>
2023-01-09 18:19:49 +0300

    feat: add info details for product

--- src/components/details/details-blockText.ts
+++ src/components/details/details-blockText.ts
@@ -1,6 +1,12 @@
 import Block from "../../core/templates/block";
 import TitleProduct from "../cart/cart-blockProduct-component/block-text/title-product";
 import DescriptionProduct from "../cart/cart-blockProduct-component/block-text/decription-product";
+import TitleLine from "../cart/cart-blockProduct-component/block-text/title-line";
+import DiscountProduct from "../cart/cart-blockProduct-component/block-text/discount-product";
+import RatingProduct from "../cart/cart-blockProduct-component/block-text/rating-product";
+import StockProduct from "./stock-details";
+import BrandProduct from "./brand-details";
+import PriceProduct from "./price-details";
 
 class DetailsText extends Block {
     constructor(id: string, className: string) {
@@ -9,7 +15,13 @@ class DetailsText extends Block {
 
     render(i: number): HTMLElement {
         this.container.append((new TitleProduct('details_title', 'details__title')).render(i));
+        this.container.append((new BrandProduct('brand_product', 'brand__product')).render(i));
         this.container.append((new DescriptionProduct('details_description', 'details__description')).render(i));
+        this.container.append((new TitleLine('1_line_details', 'line__details')).render());
+        this.container.append((new DiscountProduct('discount_product', 'discount__product')).render(i));
+        this.container.append((new RatingProduct('rating_product', 'rating__product')).render(i));
+        this.container.append((new StockProduct('stock_product', 'stock__product')).render(i));
+        this.container.append((new PriceProduct('price_product', 'price__product')).render(i));
 
         return this.container;
       }
---
+++ src/components/details/stock-details.ts
@@ -0,0 +1,20 @@
+import products from '../../../src/data.json';
+
+class StockProduct {
+  protected container: HTMLElement;
+  constructor(id: string, className: string) {
+    this.container = document.createElement('div');
+    this.container.id = id;
+    this.container.className = className;
+  }
+
+  render(i: number) {
+    for (let j = 0; j < products.length; j++) {
+      if (Number(products[j].id) === i + 1) {
+        this.container.innerText = `Stock: ${products[j].stock}`;
+      }
+    }
+    return this.container;
+  }
+}
+export default StockProduct;
