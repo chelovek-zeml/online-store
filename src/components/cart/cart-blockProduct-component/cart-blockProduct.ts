2daa0602e21b2e1c42c43b3facb28c47f94d48b2
chelovek-zeml <stranger37561@gmail.com>
2023-03-28 16:28:53 +0300

    feat: added pagination to cart

--- src/components/cart/cart-blockProduct-component/cart-blockProduct.ts
+++ src/components/cart/cart-blockProduct-component/cart-blockProduct.ts
@@ -12,11 +12,14 @@ class CartBlockProduct extends Block {
     super(id, className);
   }
 
-  render(i: number, num: number): HTMLElement {
+  render(i: number, num: number, str?: string): HTMLElement {
     this.container.append(this.productImg.render(i - 1, num));
     this.container.append(this.cartText.render(i - 1));
     this.container.append(this.cartCount.render(i));
 
+    if(str !== null) {
+      this.container.style.display = str!;
+    }
     return this.container;
   }
 }
